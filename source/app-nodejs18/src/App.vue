28a727b71d50ddf1284979d74cfccec6bee5d1ed
ejedevopsprod <73623490+ejedevopsprod@users.noreply.github.com>
2024-10-04 11:12:01 -0600

    Update App.vue

--- source/app-nodejs18/src/App.vue
+++ source/app-nodejs18/src/App.vue
@@ -1,13 +1,8 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
   <div>
     <h1>{{ title }}</h1>
     <p>La API está en: {{ apiUrl }}</p>
-    <p v-if="featureFlag">¡La funcionalidad está activada!!</p>
+    <p v-if="featureFlag">¡La funcionalidad está activada!</p>
   </div>
 </template>
 
@@ -22,33 +17,3 @@ export default {
   },
 };
 </script>
-
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
